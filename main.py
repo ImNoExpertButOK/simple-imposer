0d43b15aec8111ff822ec96b4c2cee4d5eefb276
ImNoExpertButOK <66391521+ImNoExpertButOK@users.noreply.github.com>
2024-11-12 08:22:58 -0300

    split option can now specify length
    --split now takes an int as a length of pages each signature will have.

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--combine", help="Combine different PDF files onto one.", action="store_true")
     parser.add_argument("-f", "--fill", help="Add blank pages to the end of the PDF file until the number of pages is divisible by four.", action="store_true")
     parser.add_argument("-i", "--impose", help="Impose a single PDF file.", action="store_true")
-    parser.add_argument("-s", "--split", help="Split a PDF file into signatures.", action="store_true")
+    parser.add_argument("-s", "--split", help="Split a PDF file into signatures.", nargs="?", default=32, type=int)
     parser.add_argument("files", nargs=REMAINDER)
 
     args = parser.parse_args()
 
     for file in args.files:
-        # print(file)
         pdf = Imposer(file)
 
-        pdf.split(6)
+        # pdf.split(args.split)
 
     # file = Imposer(file)
     # file.fill()
